e76a830ed74cb6517acae67ca0e4f33d26a6245d
Gustavo Lino <137111588+GutLuiz@users.noreply.github.com>
2023-10-19 14:09:45 -0300

    Add files via upload

---
+++ Program/src/Principal/Main.java
@@ -0,0 +1,34 @@
+package Principal;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import 	Conta.Fatura;
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Fatura fatura;
+		
+		System.out.print("Digite o numero da fatura: ");
+		int numero = sc.nextInt();
+		
+		System.out.print("fale a quantidade do produto: ");
+		int quantidade = sc.nextInt();
+		
+		System.out.print("Digite o preço de cada produto: ");
+		double preço = sc.nextDouble();
+		
+	    fatura = new Fatura(numero, quantidade, preço);
+		
+	    System.out.println();
+	    System.out.println("Fatura finalizada:");
+	    System.out.println(fatura); 
+	    System.out.printf("No total de: %.2f%n" , fatura.getTotalFatura() ); 
+	    
+	    
+		
+		sc.close();
+	}
+}
